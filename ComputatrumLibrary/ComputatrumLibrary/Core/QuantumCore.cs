32dde57674761978c42722532825a41d4d544061
tohoff82\admin <roman.tymokhov@gmail.com>
2017-09-16 14:28:39 +0300

    Initialize main folders and classes

---
+++ ComputatrumLibrary/ComputatrumLibrary/Core/QuantumCore.cs
@@ -0,0 +1,13 @@
+﻿using ComputatrumLibrary.Core.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputatrumLibrary.Core
+{
+    class QuantumCore : ICore
+    {
+    }
+}
---
+++ ComputatrumLibrary/ComputatrumLibrary/Drive/SsdDrive.cs
@@ -0,0 +1,13 @@
+﻿using ComputatrumLibrary.Drive.DriveInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputatrumLibrary.Drive
+{
+    class SsdDrive : IDrive
+    {
+    }
+}
