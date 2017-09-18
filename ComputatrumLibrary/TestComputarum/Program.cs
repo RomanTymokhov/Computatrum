dfb367a7160de8a7696c2dd2c3f011aaacfa7c76
tohoff82\admin <roman.tymokhov@gmail.com>
2017-09-18 22:39:09 +0300

    Create testApp for library

---
+++ ComputatrumLibrary/TestComputarum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputatrumLibrary.Core;
+
+namespace TestComputarum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleCore core1 = new SingleCore(122);
+            Console.WriteLine(core1.coreCapacity);
+            core1.AddIgnisToCoreCapacity(100);
+            Console.WriteLine(core1.coreCapacity);
+        }
+    }
+}
